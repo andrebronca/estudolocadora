a99710d480baaff26eaa0d813b6abadd56308a59
André R. B <bronca.andre@gmail.com>
2023-08-14 22:21:30 -0300

    Criação da primeira classe que referencia a tabela categoria. Essa tabela não terá chave estrangeira. A classe DAO que terá os métodos CRUD foi iniciada.

--- src/com/livro/capitulo3/categoria/Categoria.java
+++ src/com/livro/capitulo3/categoria/Categoria.java
@@ -1,5 +1,60 @@
 package com.livro.capitulo3.categoria;
 
-public class Categoria {
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categoria")
+public class Categoria implements Serializable {
+	
+	private static final long serialVersionUID = -6658831328735473555L;
+	
+	@Id
+	@GeneratedValue
+	@Column(name = "cod_categoria")
+	private Integer categoria;
+	
+	private String descricao;
+
+	public Integer getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Integer categoria) {
+		this.categoria = categoria;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(categoria, descricao);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(categoria, other.categoria) && Objects.equals(descricao, other.descricao);
+	}
+	
+	
 
 }
--- src/com/livro/capitulo3/categoria/CategoriaDAO.java
+++ src/com/livro/capitulo3/categoria/CategoriaDAO.java
@@ -1,5 +1,17 @@
 package com.livro.capitulo3.categoria;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 public class CategoriaDAO {
 
+	private Session sessao;
+	private Transaction transacao;
+	
+	public void salvar(Categoria categoria) {
+		//pg 149
+	}
+	
+	
+	
 }
